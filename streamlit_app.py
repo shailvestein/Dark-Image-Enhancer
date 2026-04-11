2b105f93dad4577ffbce713e12ea3521d30f86a2
shailvestein <62176093+shailvestein@users.noreply.github.com>
2026-04-12 00:35:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,8 +77,7 @@ if uploaded_file is not None:
         # --- PROCESSING ---
         with st.status("🚀 AI Engine is working...", expanded=True) as status:
             enhc_img, p_time = enhancer.enhance_image(img_input)
-            emhc_img = torch.clamp(emhc_img, 0, 1)
-            emhc_img = enhc_img * 255
+            enhc_img = torch.clamp(enhc_img, 0, 1)
             status.update(label=f"✨ Magic Done in {p_time:.2f}s!", state="complete", expanded=False)
     
         # --- DISPLAY ---
